7f8e412c08b6ab689536003d9a5a378422371b42
戸部拓人 <tobetch@tobetch-MBP.local>
2020-04-12 13:31:28 +0900

    fix gitignore_grobal

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ln -sf ~/git/dotfiles/.pryrc ~/.pryrc
 ln -sf ~/git/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/git/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/git/dotfiles/.gitignore_global ~/.gitignore_global
